3576b7a9dd50a68b3225a06b3cbf539673c3bfef
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-18 06:44:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
-  
+
   name_hash.each do |key,value|
     if lowest_value == 0 || value < lowest_value
-     lowest_value = value 
+     lowest_value = value
      lowest_key = key
    end
  end
